6d0db49c505c64fe731177372c4a284858d21060
luck_zr <529130510@qq.com>
2015-09-19 22:02:34 +0800

    init structure

---
+++ index.js
@@ -0,0 +1,41 @@
+/**
+ * Created by zhangran on 15/9/19.
+ */
+
+/**
+ *  引入Module
+  * @type {*|exports|module.exports}
+ */
+var express = require('express');
+
+//创建实例应用
+var app = express();
+
+//设置模版引擎
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+//静态资源目录
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/bower_components'));
+
+app.get('/',function(req,res){
+    res.render('index');
+});
+
+//500
+app.use(function(err, req, res, next){
+    if (!module.parent) console.error(err.stack);
+    res.status(500).render('500');
+});
+
+//404
+app.use(function(req, res, next){
+    res.status(404).render('404', { url: req.originalUrl });
+});
+
+/* istanbul ignore next */
+if (!module.parent) {
+    app.listen(3000);
+    console.log('Express started on port 3000');
+}
